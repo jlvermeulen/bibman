5ebf434df09d86deb3bc45c03708b114adfbb1d3
Jordi Vermeulen <jordi@jordivermeulen.nl>
2017-08-07 11:50:10 +0200

    Allow setting of working directory for the application

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-import os.path
+import os, os.path, argparse
 
 settings =  {
                 'main_window_geometry': '1280x720+100+100',
@@ -25,3 +25,10 @@ def get(name):
 
 def set(name, value):
     settings[name] = value
+
+parser = argparse.ArgumentParser(description = 'Bibliography management utility.', prog = 'bibman')
+parser.add_argument('--data-dir', default = '.', help = 'The directory in which the database, PDFs and settings are stored.')
+parser.add_argument('--version', action = 'version', version = '%(prog)s 0.1')
+
+arguments = parser.parse_args()
+os.chdir(arguments.data_dir)
